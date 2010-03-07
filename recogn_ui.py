2d0b01373b64cadc63ae49ac34072c3e66fa0efc
Ben O'Steen <bosteen@gmail.com>
2010-03-07 16:22:38 +0000

    Adding the basic UI and convenience classes

---
+++ recogn_ui.py
@@ -0,0 +1,90 @@
+import web
+
+import sys
+
+import simplejson
+
+import mimetypes
+mimetypes.init()
+
+import os
+
+from recogniser import Recogniser
+
+import PIL
+
+import ImageDraw
+from web.contrib.template import render_mako
+
+from urllib import urlencode, unquote, quote
+
+from datetime import datetime
+
+import tempfile
+
+urls = (
+        '/', 'usage',
+        )
+
+app = web.application(urls, globals(), autoreload=True)
+
+
+
+# input_encoding and output_encoding is important for unicode
+# template file. Reference:
+# http://www.makotemplates.org/docs/documentation.html#unicode
+render = render_mako(
+        directories=['templates'],
+        input_encoding='utf-8',
+        output_encoding='utf-8',
+        )
+
+class usage:
+    def GET(self):
+        r = Recogniser(".")
+        web.header('Content-type','text/html; charset=utf-8', unique=True)
+        return render.usage(r = r)
+        
+    def POST(self):
+        r = Recogniser(".")
+        x = web.input(part={})
+        if x.has_key("cascade"):
+            if x['cascade'] not in r.cascades:
+                return web.notfound("Invalid cascade file selected")
+        else:
+            return web.notfound("You must supply a valid cascade file to use")
+        if x.has_key("part"):
+            path = x['part'].filename
+            ext = path.split(".")[-1]
+            tmp_fd, tmp_fname = tempfile.mkstemp(suffix="."+ext)
+            tmpfile = os.fdopen(tmp_fd, "w+b")
+            tmpfile.write(x['part'].file.read())
+            x['part'].file.close()
+            tmpfile.close()
+            if True:
+                objs = r.detect_in_image_file(tmp_fname, str(x['cascade']))
+                if x.has_key("json"):
+                    web.header('Content-type','application/json', unique=True)
+                    os.remove(tmp_fname)
+                    data = []
+                    for obj in objs:
+                       print "%(x)s %(y)s %(w)s %(h)s" % ({'x':obj.x, 'y':obj.x, 'w':obj.width, 'h':obj.height})
+                       data.append({'x':obj.x, 'y':obj.x, 'w':obj.width, 'h':obj.height})
+                    return simplejson.dumps(data)
+                else:
+                    img = PIL.Image.open(tmp_fname)
+                    drawing = ImageDraw.Draw(img)
+                    for obj in objs:
+                        print "%(x)s %(y)s %(w)s %(h)s" % ({'x':obj.x, 'y':obj.x, 'w':obj.width, 'h':obj.height})
+                        drawing.rectangle([obj.x, obj.y, obj.x+obj.width, obj.y+obj.height], outline=255, fill=128)
+                    del drawing
+                    fd_png, png_fname = tempfile.mkstemp(suffix=".png")
+                    tmpfile = os.fdopen(fd_png, "w+b")
+                    img.save(tmpfile, "PNG")
+                    os.remove(tmp_fname)
+                    tmpfile.seek(0)
+                    web.header('Content-type','image/png', unique=True)
+                    return tmpfile.read()
+
+if __name__ == "__main__":
+    app.run()
---
+++ recogniser.py
@@ -0,0 +1,70 @@
+import opencv
+from opencv import highgui
+
+import PIL
+
+import os
+
+class CascadeNotFound(Exception):
+  pass
+
+class CouldntReadAsImagefile(Exception):
+  pass
+
+class ImageFileNotFound(Exception):
+  pass
+
+class Recogniser(object):
+  def __init__(self, cascade_dir="/usr/local/share/opencv/haarcascades"):
+    self._cached_cascades = {}
+    self.cascade_dir = cascade_dir
+    self.register_cascades()
+
+  def register_cascades(self):
+    self.cascades = []
+    for cached_cascade in self._cached_cascades:
+      del self._cached_cascades[cached_cascade]
+    if os.path.isdir(self.cascade_dir):
+      for file in [x for x in os.listdir(self.cascade_dir) if x.endswith(".xml")]:
+        self.cascades.append(file)
+
+  def get_cascade(self, cascade_name):
+    self._cached_cascades[cascade_name] = opencv.cvLoadHaarClassifierCascade(
+            os.path.join(self.cascade_dir, cascade_name), opencv.cvSize(1,1))
+            #cascade_name, opencv.cvSize(1,1))
+    return self._cached_cascades[cascade_name]
+
+
+  def detect_in_image_file(self, filename, cascade_name, recogn_w = 50, recogn_h = 50):
+    if os.path.isfile(filename):
+      try:
+        pil_image = PIL.Image.open(filename)
+      except:
+        raise CouldntReadAsImagefile
+      return self.detect(pil_image, cascade_name, recogn_w, recogn_h)
+    else:
+      raise ImageFileNotFound
+
+  def detect(self, pil_image, cascade_name, recogn_w = 50, recogn_h = 50):
+    # Get cascade:
+    cascade = self.get_cascade(cascade_name)
+
+    image = opencv.PIL2Ipl(pil_image) 
+    image_size = opencv.cvGetSize(image)
+    grayscale = image
+    if pil_image.mode == "RGB": 
+      # create grayscale version
+      grayscale = opencv.cvCreateImage(image_size, 8, 1)
+      # Change to RGB2Gray - I dont think itll affect the conversion
+      opencv.cvCvtColor(image, grayscale, opencv.CV_BGR2GRAY)
+ 
+    # create storage
+    storage = opencv.cvCreateMemStorage(0)
+    opencv.cvClearMemStorage(storage)
+ 
+    # equalize histogram
+    opencv.cvEqualizeHist(grayscale, grayscale)
+ 
+    # detect objects
+    return opencv.cvHaarDetectObjects(grayscale, cascade, storage, 1.2, 2, opencv.CV_HAAR_DO_CANNY_PRUNING, opencv.cvSize(recogn_w, recogn_h))
+
